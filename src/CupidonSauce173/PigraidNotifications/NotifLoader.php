7aac163ceb21b43729a06261da288fbfbd9ab5d7
CupidonSauce173 <CupidonSauce173@gmail.com>
2021-06-27 07:44:14 -0400

    Removed CheckDisplayed class.
    Removed CheckDisplayed class and implemented it directly in NotifLoader instead.

--- src/CupidonSauce173/PigraidNotifications/NotifLoader.php
+++ src/CupidonSauce173/PigraidNotifications/NotifLoader.php
@@ -15,6 +15,7 @@ use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\ClosureTask;
+use pocketmine\scheduler\Task;
 use pocketmine\utils\Config;
 
 use Thread;
@@ -90,7 +91,23 @@ class NotifLoader extends PluginBase implements Listener
         ), $this->config['check-database-task'] * 20);
 
         # Task to check if the notifications has been displayed to the player.
-        $this->getScheduler()->scheduleRepeatingTask(new CheckDisplayed(), $this->config['check-displayed-task'] * 20);
+        $this->getScheduler()->scheduleRepeatingTask(new class extends Task{
+            public function onRun(int $currentTick)
+            {
+                foreach(NotifLoader::getInstance()->getServer()->getOnlinePlayers() as $player){
+                    if(!isset(NotifLoader::getInstance()->notificationList[$player->getName()])) return;
+                    /** @var Notification $notification */
+                    foreach (NotifLoader::getInstance()->notificationList[$player->getName()] as $notification) {
+                        if ($notification->hasBeenDisplayed() !== true) {
+                            var_dump('CheckDisplayed Task object ID: ' . spl_object_id($notification));
+                            $player = NotifLoader::getInstance()->getServer()->getPlayer($notification->getPlayer());
+                            $player->sendMessage(NotifLoader::getInstance()->TranslateNotification($notification));
+                            $notification->setDisplayed(true);
+                        }
+                    }
+                }
+            }
+        }, $this->config['check-displayed-task'] * 20);
 
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->form = $this->getServer()->getPluginManager()->getPlugin('FormAPI');
